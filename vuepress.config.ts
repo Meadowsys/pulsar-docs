188fabef87429c5c44d05a5fa33cd747afab2c7a
Autumn Meadow <blazeykirin@gmail.com>
2022-07-17 14:40:39 -0700

    un integrate docsearch plugin
    figure this out later :p

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -1,6 +1,5 @@
 import { defineUserConfig, defaultTheme } from "vuepress";
 import { shikiPlugin } from "@vuepress/plugin-shiki";
-import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 
 export default defineUserConfig({
 	port: 3000,
@@ -21,12 +20,6 @@ export default defineUserConfig({
 		}
 	}),
 	plugins: [
-		shikiPlugin({ theme: "one-dark-pro" }),
-		docsearchPlugin({
-			appId: "GKVR1BYYSI",
-			apiKey: "b0784104edc7fc3b010f69139b964a20",
-			indexName: "pulsar"
-			// todo dutch locale?
-		})
+		shikiPlugin({ theme: "one-dark-pro" })
 	]
 });
