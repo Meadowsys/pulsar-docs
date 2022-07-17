f014c30cac8e908ce94f90d80c671d7bfc1cf76c
Autumn Meadow <blazeykirin@gmail.com>
2022-07-17 14:29:14 -0700

    integrate docsearch plugin

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig, defaultTheme } from "vuepress";
 import { shikiPlugin } from "@vuepress/plugin-shiki";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 
 export default defineUserConfig({
 	port: 3000,
@@ -20,6 +21,12 @@ export default defineUserConfig({
 		}
 	}),
 	plugins: [
-		shikiPlugin({ theme: "one-dark-pro" })
+		shikiPlugin({ theme: "one-dark-pro" }),
+		docsearchPlugin({
+			appId: "GKVR1BYYSI",
+			apiKey: "b0784104edc7fc3b010f69139b964a20",
+			indexName: "pulsar"
+			// todo dutch locale?
+		})
 	]
 });
